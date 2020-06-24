7a03bd7cefb9b4c929bcd45400396b6de40b1163
frankcbliu <641259875@qq.com>
2020-06-24 23:39:58 +0800

    init

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * USER: frankcbliu
+ * DATE: 2020/6/21
+ * TITLE:
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
